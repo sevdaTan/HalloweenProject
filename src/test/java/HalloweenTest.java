037fd7c01ecc16c2738a896c17f2e0fe943fc286
sevdaTan <sevdatanyildizi@gmail.com>
2020-10-31 12:43:02 -0400

    halloween updated

--- src/test/java/HalloweenTest.java
+++ src/test/java/HalloweenTest.java
@@ -3,4 +3,6 @@ public class HalloweenTest {
     // I will give everyone to some candy
     // Are we there yet?
     // today kids will do treat or trick ^_^ Booooo
+
+    // don't forget to wear your masks...
 }
